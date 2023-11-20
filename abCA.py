becddb847aea0dab04f85ee568e873c587af424d
everyoneisgross <fraser.austin@gmail.com>
2023-11-20 14:41:29 +1300

    🖇

--- abCA.py
+++ abCA.py
@@ -2,15 +2,13 @@
 Script ranks steps of the Abrameln Rites based on user input and distributes them over a grid for running CA over producing generative ritual instructions.
 Riffing on ancient magical practice as computation, written frameworks for real life problem solving, or oral spells one can rationalise with etc.
 """
-
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 import matplotlib.pyplot as plt
-import imageio
+import imageio.v2 as imageio
 import os
 import datetime
-
-similarity_threshold = 0.5
+from gpt4all import GPT4All, Embed4All
 
 # Abramelin Ritual descriptions
 ritual_descriptions = [
@@ -32,46 +30,33 @@ ritual_descriptions = [
     "Gradually close the magical circle.",
     "Reflect on the experience and integrate insights into daily life."
 ]
+def generate_embeddings(texts):
+    embedder = Embed4All()
+    embeddings = [embedder.embed(text) for text in texts]
+    return embeddings
 
-# Function to map descriptions to a vector
-def map_descriptions_to_vector(descriptions, input_text):
-    vectors = []
-    for description in descriptions:
-        vector = np.zeros(len(descriptions), dtype=float)
-        for word in description.split():
-            if word in input_text.split():
-                vector[descriptions.index(description)] += 1
-        vectors.append(vector)
-    return vectors
-
-# Function to initialize the grid with each cell representing a different description
-def initialize_grid(descriptions, input_text, grid_width, grid_height):
-    input_vector = map_descriptions_to_vector(descriptions, input_text)
-    similarity_scores = [cosine_similarity([input_vector[0]], [map_descriptions_to_vector(descriptions, desc)[0]])[0][0] for desc in descriptions]
-
-    ca_grid = np.zeros((grid_height, grid_width), dtype=int)
-    for row in range(grid_height):
-        for col in range(grid_width):
-            desc_index = (row * grid_width + col) % len(descriptions)
-            # Activate cell if corresponding description has high similarity
-            ca_grid[row, col] = 1 if similarity_scores[desc_index] > similarity_threshold else 0
-
-    return ca_grid
+def compute_similarity_scores(input_embedding, description_embeddings):
+    return [cosine_similarity([input_embedding], [desc_emb])[0][0] for desc_emb in description_embeddings]
 
 def ca_step_2d(grid, rule):
-    new_grid = np.zeros_like(grid)
+    new_grid = np.empty_like(grid)
     grid_height, grid_width = grid.shape
 
-    for row in range(grid_height - 1):  # Evolve from top to bottom
+    for row in range(grid_height):
         for col in range(grid_width):
-            # Using modulo for wrapping around the grid
-            left = grid[row, (col - 1) % grid_width]
-            center = grid[row, col]
-            right = grid[row, (col + 1) % grid_width]
+            # Extract state and description from each neighboring cell
+            state_above, _ = grid[(row - 1) % grid_height, col]
+            state_left, _ = grid[row, (col - 1) % grid_width]
+            state_center, description = grid[row, col] # Keep the description from the center cell
+            state_right, _ = grid[row, (col + 1) % grid_width]
+            state_below, _ = grid[(row + 1) % grid_height, col]
 
-            # Calculating the pattern with wrapping
-            pattern = 4 * left + 2 * center + right
-            new_grid[row + 1, col] = rule[7 - pattern]
+            # Calculating the pattern with wrapping for both rows and columns
+            pattern = (state_above << 3) + (state_left << 2) + (state_center << 1) + state_right + state_below
+            new_state = rule[pattern % len(rule)]
+
+            # Update the new grid with the new state and the original description
+            new_grid[row, col] = [new_state, description]
 
     return new_grid
 
@@ -80,35 +65,36 @@ def ca_step_2d(grid, rule):
 def get_wolfram_rule(rule_number):
     return [int(x) for x in format(rule_number, '08b')]
 
-# Function to initialize the grid with each cell representing a different description
-def initialize_grid_with_descriptions(descriptions, input_text, grid_size):
-    input_vector = map_descriptions_to_vector(descriptions, input_text)
-    similarity_scores = [cosine_similarity([input_vector[0]], [map_descriptions_to_vector(descriptions, desc)[0]])[0][0] for desc in descriptions]
+def initialize_grid_with_descriptions(descriptions, similarity_scores, grid_size, rank_descriptions=True):
+    # Rank descriptions based on similarity if required
+    if rank_descriptions:
+        ranked_descriptions = [desc for _, desc in sorted(zip(similarity_scores, descriptions), reverse=True)]
+    else:
+        ranked_descriptions = descriptions
 
-    ca_grid = np.zeros((grid_size, grid_size), dtype=int)
+    # Initialize the grid
+    ca_grid = np.empty((grid_size, grid_size), dtype=object)
 
-    # Initialize the grid with descriptions based on similarity
-    for row in range(grid_size):
-        for col in range(grid_size):
-            desc_index = (row * grid_size + col) % len(descriptions)
-            ca_grid[row, col] = 1 if similarity_scores[desc_index] > similarity_threshold else 0
-
-    # Introduce an initial active cell in the middle of the first row
-    ca_grid[0, grid_size // 2] = 1
+    # Populate the grid with descriptions
+    for i in range(grid_size * grid_size):
+        row, col = divmod(i, grid_size)
+        # Use modulo to loop through the ranked_descriptions
+        ca_grid[row, col] = [1, ranked_descriptions[i % len(ranked_descriptions)]] if i < len(ranked_descriptions) else [0, ranked_descriptions[i % len(ranked_descriptions)]]
 
     return ca_grid
 
-# Save grid state as an image
+
 def save_grid_state_as_image(grid, filename):
-    plt.imshow(grid, cmap='binary')
+    # Extract the state (0 or 1) from each cell for visualization
+    state_grid = np.array([[cell[0] for cell in row] for row in grid])
+    plt.imshow(state_grid, cmap='binary')
     plt.axis('off')
     plt.savefig(filename)
     plt.close()
 
-# Run and save the CA evolution
 def run_and_save_ca_2d(input_text, descriptions, rule_number, grid_size, ca_steps):
     ca_rule = get_wolfram_rule(rule_number)
-    ca_grid = initialize_grid_with_descriptions(descriptions, input_text, grid_size)
+    ca_grid = initialize_grid_with_descriptions(descriptions, input_text, grid_size, rank_choice.lower() == 'yes')
 
     filenames = []
     all_ritual_steps = []
@@ -118,9 +104,9 @@ def run_and_save_ca_2d(input_text, descriptions, rule_number, grid_size, ca_step
     all_ritual_steps.append(f"Rule Number: {rule_number}\n\n")
 
     for step in range(ca_steps):
-        # Record active descriptions
-        active_sentences = [descriptions[(row * grid_size + col) % len(descriptions)] for row in range(grid_size) for col in range(grid_size) if ca_grid[row, col] == 1]
-        all_ritual_steps.append(f"Step {step + 1}: " + ", ".join(active_sentences) + "\n")
+        # Ensure that descriptions are converted to strings, and use a placeholder for None
+        grid_state = "\n".join([" ".join([str(cell[1]) if cell[0] == 1 else '.' for cell in row]) for row in ca_grid])
+        all_ritual_steps.append(f"Step {step + 1}:\n{grid_state}\n\n")
 
         filename = f"ca_step_{step}.png"
         save_grid_state_as_image(ca_grid, filename)
@@ -143,10 +129,28 @@ def run_and_save_ca_2d(input_text, descriptions, rule_number, grid_size, ca_step
     print(f"CA evolution saved to '{gif_filename}'")
     print(f"Ritual evolution saved to '{txt_filename}'")
 
+
+# Main execution
 if __name__ == "__main__":
     user_input = input("Enter a sentence related to the Abramelin Ritual: ")
     rule_number = int(input("Enter a Wolfram CA rule number (0-255): "))
     grid_size = int(input("Enter the grid size: "))
     ca_steps = int(input("Enter the number of CA steps: "))
+    rank_choice = input("Rank descriptions based on similarity? (yes/no): ")
+
+    # Embeddings and similarity calculation (only once)
+    all_texts = [user_input] + ritual_descriptions
+    all_embeddings = generate_embeddings(all_texts)
+    input_embedding = all_embeddings[0]
+    description_embeddings = all_embeddings[1:]
+    similarity_scores = compute_similarity_scores(input_embedding, description_embeddings)
+
+    # Initialize grid with descriptions based on user choice
+    rank_descriptions = rank_choice.lower() == 'yes'
+    ca_grid = initialize_grid_with_descriptions(ritual_descriptions, similarity_scores, grid_size, rank_descriptions)
+
+    # Run and save CA
+    ca_rule = get_wolfram_rule(rule_number)
     run_and_save_ca_2d(user_input, ritual_descriptions, rule_number, grid_size, ca_steps)
+
 
